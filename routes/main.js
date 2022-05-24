edc41b8430f7c8991ce26832c91f3833e653b968
43213010019 <106139346+43213010019@users.noreply.github.com>
2022-05-24 19:44:32 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -43,8 +43,8 @@ var port = process.env.PORT || 8080 || 5000 || 3000
         uptime: muptime(process.uptime()),
         speed: `${neww - old}ms`,
         info:{       
-            owner: 'alpin',            
-            apikey: 'Chat Owner: https://wa.me/6289690719275'
+            owner: 'Ronaldo',            
+            apikey: 'Chat Owner: https://wa.me/62895803299969'
         }
     }
     res.json(status)
